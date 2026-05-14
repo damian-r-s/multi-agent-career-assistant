bd7d229d0fbe21178a84f1e1e0d0d4931355b1cd
Damian Sobanski <d.sobanski@hotmail.com>
2026-05-14 14:50:47 +0200

    rename web client

--- test/test_tools.py
+++ test/test_tools.py
@@ -1,7 +1,7 @@
 import os
 from unittest.mock import Mock, patch
 
-from tools import file_reader, github_api, job_description_client
+from tools import file_reader, github_api, job_tavil_client
 
 def test_read_file_existing(tmp_path):
     sample_file = tmp_path / "sample.txt"
